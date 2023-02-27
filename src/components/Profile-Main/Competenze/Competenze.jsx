1047a3a9207ff7650d7d7330865fd4cbd8bf71fd
shaxa321 <amonovshahruz@gmail.com>
2023-02-27 20:08:41 +0100

    ok

---
+++ src/components/Profile-Main/Competenze/Competenze.jsx
@@ -0,0 +1,115 @@
+import { Col, Row, Container, Button } from "react-bootstrap";
+import { useState } from "react";
+import { AiOutlinePlus, AiOutlineArrowLeft } from "react-icons/ai";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import "../../../App.css";
+import "./app.scss";
+
+const Competenze = () => {
+  const MAXIMUM = 100;
+
+  const DEFAULTNUMBER = 3;
+  const [numberToDisplay, setNumberToDisplay] = useState(DEFAULTNUMBER);
+
+  const dataCompetenze = [
+    {
+      titolo: "Angular",
+      Conferme: ["Lucia Dolla", "Pinco Pallino", "Sara Dello Russo"],
+      Descrizione: "Student presso Epicode Italia",
+      Categoria: "Conoscenza del Settore",
+      img: "https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/EPICODE_resized/original.png?1651791536",
+    },
+    {
+      titolo: "Javascript",
+      Descrizione: "Student presso Epicode Italia",
+      Conferme: ["Lucia Dolla", "Pinco Pallino", "Sara Dello Russo"],
+      Categoria: "Conoscenza del Settore",
+      img: "https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/EPICODE_resized/original.png?1651791536",
+    },
+    {
+      titolo: "Forbes",
+      Descrizione: "Esperto di Profumi",
+      Conferme: ["Hugo Boss", "Chartier", "Parlament", "Lamborghini"],
+      img: "https://logos-world.net/wp-content/uploads/2021/08/Forbes-Symbol.png",
+      Categoria: "Conoscenza del Settore",
+    },
+    {
+      titolo: "Css",
+      Conferme: ["Bootstrap CSS"],
+      Descrizione: "Student presso Epicode Italia",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+      Categoria: "Conoscenza del Settore",
+    },
+    {
+      titolo: "Investigazione Privata",
+      Descrizione: "6 mesi Scuola di Polizia",
+      Conferme: ["POLIZIA DI STATO"],
+      img: "https://thumbs.dreamstime.com/b/police-officer-badge-icon-vector-89817911.jpg",
+      Categoria: "Investigazione",
+    },
+  ];
+
+  return (
+    <Container className="allCards">
+      <Row>
+        <Col className="d-flex align-items-center">
+          {numberToDisplay > DEFAULTNUMBER ? (
+            <AiOutlineArrowLeft
+              className="iconTitle"
+              onClick={(e) => setNumberToDisplay(DEFAULTNUMBER)}
+            />
+          ) : (
+            <></>
+          )}
+          <h5>Competenze</h5>
+        </Col>
+      </Row>
+
+      {dataCompetenze.map((element, index) => {
+        if (index < numberToDisplay)
+          return (
+            <ul>
+              <h6> {element.titolo}</h6>
+
+              <li>
+                <img
+                  className="imgIcon"
+                  src={element.img}
+                  alt={element.titolo + " immagine"}
+                />
+                {element.Descrizione}
+              </li>
+              <li>
+                {" "}
+                <BsFillPeopleFill className="imgIcon" />
+                <a href="">{element.Conferme.length + " Conferme"}</a>
+              </li>
+            </ul>
+          );
+      })}
+      {numberToDisplay <= DEFAULTNUMBER ? (
+        <Row
+          className="changeHoverColor"
+          onClick={() => {
+            setNumberToDisplay(MAXIMUM);
+          }}
+        >
+          <Col className="d-flex justify-content-center align-items-center">
+            {dataCompetenze.length > DEFAULTNUMBER ? (
+              <p>
+                Mostra tutte le Competenze({dataCompetenze.length}){" "}
+                <BsFillArrowRightCircleFill />
+              </p>
+            ) : (
+              <></>
+            )}
+          </Col>
+        </Row>
+      ) : (
+        <></>
+      )}
+    </Container>
+  );
+};
+export default Competenze;
--- src/components/Profile-Main/ProfileMain.jsx
+++ src/components/Profile-Main/ProfileMain.jsx
@@ -6,6 +6,7 @@ import Attività from "./Attività/Attività";
 import Esperienza from "./Esperienza/Esperienza";
 import Informazioni from "./Informazioni/Informazioni";
 import Risorse from "./Risorse/Risorse";
+import Competenze from "./Competenze/Competenze";
 import { Learning } from "../Learning/Learing";
 import { OtherCompanies } from "../Other-Companies/OtherCompanies";
 import { OtherPeople } from "../Other-People/OtherPeople";
@@ -23,6 +24,7 @@ export const ProfileMain = () => {
           <Attività />
           <Esperienza />
           {/* parte a sinistra */}
+          <Competenze />
           <Interessi />
         </Col>
         <Col md={3} className="d-none d-md-block">
