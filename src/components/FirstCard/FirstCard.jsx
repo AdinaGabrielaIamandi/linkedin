de54bd59178f2a26c05d687e9623505a46337b66
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-28 13:09:20 +0100

    scss first card & navbar

--- src/components/FirstCard/FirstCard.jsx
+++ src/components/FirstCard/FirstCard.jsx
@@ -1,9 +1,10 @@
 import { Card, Image, Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./app.scss";
+import "./firstCard.scss";
 import { FaTelegramPlane } from "react-icons/fa";
 import { HiOutlinePencil } from "react-icons/hi";
 import { useState } from "react";
+import Cover from "../../images/cover-foto.png";
 
 export const FirstCard = (props) => {
   const [show, setShow] = useState(false);
@@ -13,7 +14,7 @@ export const FirstCard = (props) => {
 
   return (
     <Card className="mb-2">
-      <Card.Img variant="top" src="http://placekitten.com/200/150" style={{ width: "100%", height: "180px" }} />
+      <Card.Img variant="top" src={Cover} style={{ width: "100%", height: "180px" }} />
       <Card.Body>
         <Image
           src={props.image}
@@ -41,12 +42,11 @@ export const FirstCard = (props) => {
             </Modal.Footer>
           </Modal>
           <div className="d-flex align-items-baseline mt-3">
-            <h4 className="m-0">
+            <h4 className="m-0 text-black">
               {props.name} {props.surname}
             </h4>
-            <p className="mx-2 m-0"> • 1°</p>
           </div>
-          <p className="mb-2">{props.work}</p>
+          <p className="mb-2 text-black work">{props.work}</p>
           <p className="mb-2 text-secondary">
             {props.town} •
             <span>
@@ -57,20 +57,12 @@ export const FirstCard = (props) => {
           </p>
           <p>
             <Link to="/" className="text-primary fw-bold text-decoration-none link mb-2">
-              Num collegamenti
-            </Link>
-          </p>
-          <p>
-            <Link to="/" className="text-secondary text-decoration-none link2 mb-2">
-              img col. <span className="fw-bold">nome cognome</span>,<span className="fw-bold"> nome cognome</span> e
-              altri 2 collegamenti in comune
+              12 collegamenti
             </Link>
           </p>
         </Card.Text>
         <div className="d-flex mb-3">
-          <Button className="cta2 fw-bold me-2 ">
-            <FaTelegramPlane /> Messaggio
-          </Button>
+          <Button className="cta2 fw-bold me-2 ">Disponibile per</Button>
           <Button className="cta fw-bold">Altro</Button>
         </div>
       </Card.Body>
