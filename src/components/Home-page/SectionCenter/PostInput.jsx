1263decfa0f4f8cb4743d80e5b6bdc63ed0d8ee3
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-01 11:47:50 +0100

    input done

--- src/components/Home-page/SectionCenter/PostInput.jsx
+++ src/components/Home-page/SectionCenter/PostInput.jsx
@@ -1,3 +1,25 @@
+import { Card, InputGroup, Form } from "react-bootstrap";
+import "./PostInput.scss";
+
 export const PostInput = () => {
-  return;
+  return (
+    <Card className="p-3">
+      <div className="d-flex">
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
+            alt=""
+          />
+        </div>
+
+        <InputGroup className="mb-3 ms-3 ">
+          <Form.Control
+            className="fw-bold"
+            id="inputPost"
+            placeholder="Avvia un post"
+          />
+        </InputGroup>
+      </div>
+    </Card>
+  );
 };
