cbb396d121c87bbc44b86acbd6d9792e2a35901d
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-01 12:20:50 +0100

    Header done

--- src/components/Home-page/SectionRight/SectionRight.jsx
+++ src/components/Home-page/SectionRight/SectionRight.jsx
@@ -1,3 +1,13 @@
+import { Card } from "react-bootstrap";
+import { BsFillInfoSquareFill } from "react-icons/bs";
+
 export const SectionRight = () => {
-  return;
+  return (
+    <Card className="px-2 py-3">
+      <div className="d-flex justify-content-between align-items-center">
+        <h6 className="fw-bold text-black mb-0">Linkedin Notizie</h6>
+        <BsFillInfoSquareFill style={{ width: "0.7em" }} />
+      </div>
+    </Card>
+  );
 };
