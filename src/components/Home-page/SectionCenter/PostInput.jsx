377ffe609d49b8a851fd77cab192ea8ebceda123
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-01 14:14:54 +0100

    modal component imported in input

--- src/components/Home-page/SectionCenter/PostInput.jsx
+++ src/components/Home-page/SectionCenter/PostInput.jsx
@@ -1,49 +1,68 @@
-import { Card, InputGroup, Form } from "react-bootstrap";
+import { Card, InputGroup, Form, Modal, Button } from "react-bootstrap";
 import "./PostInput.scss";
 import { AiFillPicture } from "react-icons/ai";
 import { BsFillPlayBtnFill } from "react-icons/bs";
 import { BsCalendar2Date } from "react-icons/bs";
 import { RiArticleFill } from "react-icons/ri";
+import { ModalInput } from "./ModalInput";
+import { useState } from "react";
 
 export const PostInput = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <Card className="p-3">
-      <div className="d-flex ">
-        <div>
-          <img
-            id="portraitInput"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
-            alt=""
-          />
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <ModalInput />
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Card className="p-3">
+        <div className="d-flex ">
+          <div>
+            <img
+              id="portraitInput"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
+              alt=""
+            />
+          </div>
+
+          <InputGroup className="mb-3 ms-3 ">
+            <Form.Control
+              className="fw-bold"
+              id="inputPost"
+              placeholder="Avvia un post"
+              onClick={handleShow}
+            />
+          </InputGroup>
         </div>
 
-        <InputGroup className="mb-3 ms-3 ">
-          <Form.Control
-            className="fw-bold"
-            id="inputPost"
-            placeholder="Avvia un post"
-          />
-        </InputGroup>
-      </div>
-
-      <div className="d-flex justify-content-center justify-content-lg-between iconsUnderInput flex-wrap">
-        <div className="d-flex me-4 me-lg-0">
-          <AiFillPicture className="iconFields" />
-          <p className="ms-3 fw-bold textIcon mb-0">Foto</p>
+        <div className="d-flex justify-content-center justify-content-lg-between iconsUnderInput flex-wrap">
+          <div className="d-flex me-4 me-lg-0">
+            <AiFillPicture className="iconFields" />
+            <p className="ms-3 fw-bold textIcon mb-0">Foto</p>
+          </div>
+          <div className="d-flex me-4 me-lg-0">
+            <BsFillPlayBtnFill className="iconFields green" />
+            <p className="ms-3 fw-bold textIcon mb-0">Video</p>
+          </div>
+          <div className="d-flex me-4 me-lg-0">
+            <BsCalendar2Date className="iconFields orange" />
+            <p className="ms-3 fw-bold textIcon mb-0">Evento</p>
+          </div>
+          <div className="d-flex mt-3 mt-lg-0">
+            <RiArticleFill className="iconFields red" />
+            <p className="ms-3 fw-bold textIcon mb-0">Scrivi un articolo</p>
+          </div>
         </div>
-        <div className="d-flex me-4 me-lg-0">
-          <BsFillPlayBtnFill className="iconFields green" />
-          <p className="ms-3 fw-bold textIcon mb-0">Video</p>
-        </div>
-        <div className="d-flex me-4 me-lg-0">
-          <BsCalendar2Date className="iconFields orange" />
-          <p className="ms-3 fw-bold textIcon mb-0">Evento</p>
-        </div>
-        <div className="d-flex mt-3 mt-lg-0">
-          <RiArticleFill className="iconFields red" />
-          <p className="ms-3 fw-bold textIcon mb-0">Scrivi un articolo</p>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 };
