31c8bb2994af3678d9ae4c174fedd7a54be31dc1
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-03-01 11:13:44 +0100

    impostazione lavoro

--- src/components/Home-page/Homepage.jsx
+++ src/components/Home-page/Homepage.jsx
@@ -1,13 +1,22 @@
 import { SectionLeft } from "./SectionLeft/SectionLeft";
 import { SectionCenter } from "./SectionCenter/PostInput/SectionCenter";
 import { SectionRight } from "./SectionRight/PostInput/SectionRight";
+import { Col, Container, Row } from "react-bootstrap";
 
 export const Homepage = () => {
   return (
-    <>
-      <SectionLeft />
-      <SectionCenter />
-      <SectionRight />
-    </>
+    <Container>
+      <Row className="justify-content-center">
+        <Col lg={2}>
+          <SectionLeft />
+        </Col>
+        <Col lg={6}>
+          <SectionCenter />
+        </Col>
+        <Col lg={3}>
+          <SectionRight />
+        </Col>
+      </Row>
+    </Container>
   );
 };
