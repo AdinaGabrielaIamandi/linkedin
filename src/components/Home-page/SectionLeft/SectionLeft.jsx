13da9468d6318978ec151c589d073806b872f5d2
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-03-01 16:21:19 +0100

    .

--- src/components/Home-page/SectionLeft/SectionLeft.jsx
+++ src/components/Home-page/SectionLeft/SectionLeft.jsx
@@ -4,14 +4,17 @@ import "./sectionLeft.scss";
 export const SectionLeft = () => {
   return (
     <Card>
-      <Card.Img variant="top" src="http://placekitten.com/g/48/24" />
-      <Card.Body>
-        <Image src="http://placekitten.com/g/65/65" className="rounded-circle" />
-        <Card.Title>Card Title</Card.Title>
+      <Card.Img variant="top" src="http://placekitten.com/g/48/12" />
+      <Image
+        src="http://placekitten.com/g/65/65"
+        className="rounded-circle border border-2 border-white foto-profile-first"
+      />
+      <Card.Body className=" m-0 mt-4 text-center">
+        <Card.Title className="m-0">User Name</Card.Title>
+        <small className="border-bottom-5">Posizione lavorativa</small>
         <Card.Text>
           Some quick example text to build on the card title and make up the bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
