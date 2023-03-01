265eaf7d46446d8837ab4b97307c7e02af990faf
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-01 12:15:11 +0100

    inputiField component finished

--- src/components/Home-page/SectionCenter/PostInput.jsx
+++ src/components/Home-page/SectionCenter/PostInput.jsx
@@ -1,10 +1,14 @@
 import { Card, InputGroup, Form } from "react-bootstrap";
 import "./PostInput.scss";
+import { AiFillPicture } from "react-icons/ai";
+import { BsFillPlayBtnFill } from "react-icons/bs";
+import { BsCalendar2Date } from "react-icons/bs";
+import { RiArticleFill } from "react-icons/ri";
 
 export const PostInput = () => {
   return (
     <Card className="p-3">
-      <div className="d-flex">
+      <div className="d-flex ">
         <div>
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
@@ -19,6 +23,25 @@ export const PostInput = () => {
             placeholder="Avvia un post"
           />
         </InputGroup>
+      </div>
+
+      <div className="d-flex justify-content-between">
+        <div className="d-flex">
+          <AiFillPicture className="iconFields" />
+          <p className="ms-3 fw-bold textIcon mb-0">Foto</p>
+        </div>
+        <div className="d-flex">
+          <BsFillPlayBtnFill className="iconFields green" />
+          <p className="ms-3 fw-bold textIcon mb-0">Video</p>
+        </div>
+        <div className="d-flex">
+          <BsCalendar2Date className="iconFields orange" />
+          <p className="ms-3 fw-bold textIcon mb-0">Evento</p>
+        </div>
+        <div className="d-flex">
+          <RiArticleFill className="iconFields red" />
+          <p className="ms-3 fw-bold textIcon mb-0">Scrivi un articolo</p>
+        </div>
       </div>
     </Card>
   );
