a0bf2c5c07316a7514ab2b148d14b7f62c9e6b07
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-01 14:41:34 +0100

    Header modal done

--- src/components/Home-page/SectionCenter/ModalInput.jsx
+++ src/components/Home-page/SectionCenter/ModalInput.jsx
@@ -1,25 +1,43 @@
 import { Form, Button, Modal } from "react-bootstrap";
-import { useState } from "react";
+import { BiWorld } from "react-icons/bi";
+import { RxTriangleDown } from "react-icons/rx";
+import "./PostInput.scss";
 
 export const ModalInput = () => {
   return (
     <>
       <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Title>Crea un post</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="name@example.com"
-              autoFocus
+        <div className="d-flex ms-2">
+          <div>
+            <img
+              id="portraitInput"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
+              alt=""
             />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+          </div>
+
+          <div className="mb-3 ms-2 ">
+            <h6 className="fw-bold mb-1">Mona Lisa</h6>
+            <Button className="ctaInput py-1 px-3 d-flex align-items-center fw-bold">
+              <BiWorld className="me-1 buttonIcon" /> Chiunque{" "}
+              <RxTriangleDown className="ms-1 buttonIcon" />
+            </Button>
+          </div>
+        </div>
+        <Form>
+          <Form.Group
+            className="mb-3 mt-3"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Control
+              placeholder="Di cosa vorresti parlare?"
+              className="border-0"
+              as="textarea"
+              rows={3}
+            />
           </Form.Group>
         </Form>
       </Modal.Body>
