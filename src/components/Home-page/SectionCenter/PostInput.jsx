f940fbcc72fbc116334b9fd75ccb898e1ed47f3f
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-03-02 17:20:19 +0100

    easter egg

--- src/components/Home-page/SectionCenter/PostInput.jsx
+++ src/components/Home-page/SectionCenter/PostInput.jsx
@@ -8,31 +8,23 @@ import { ModalInput } from "./ModalInput";
 import { useState } from "react";
 
 export const PostInput = (props) => {
+  console.log("props", props);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
     <>
       <Modal show={show} onHide={handleClose}>
-        <ModalInput />
+        <ModalInput name={props.name} surname={props.surname} image={props.image} />
       </Modal>
       <Card className="p-3">
         <div className="d-flex ">
           <div>
-            <img
-              id="portraitInput"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
-              alt=""
-            />
+            <img id="portraitInput" src={props.image} alt="" />
           </div>
 
           <InputGroup className="mb-3 ms-3 ">
-            <Form.Control
-              className="fw-bold"
-              id="inputPost"
-              placeholder="Avvia un post"
-              onClick={handleShow}
-            />
+            <Form.Control className="fw-bold" id="inputPost" placeholder="Avvia un post" onClick={handleShow} />
           </InputGroup>
         </div>
 
--- src/components/Home-page/SectionCenter/SectionCenter.jsx
+++ src/components/Home-page/SectionCenter/SectionCenter.jsx
@@ -1,10 +1,10 @@
 import { PostInput } from "./PostInput";
 import { PostGet } from "./PostGet";
 
-export const SectionCenter = () => {
+export const SectionCenter = (props) => {
   return (
     <>
-      <PostInput /> 
+      <PostInput name={props.name} surname={props.surname} image={props.image} />
       <PostGet />
     </>
   );
