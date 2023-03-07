fe82295a98cca6c7166e0e2ba4b548971fc47603
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-03-07 17:03:26 +0100

    filter post friends

--- src/components/Home-page/SectionCenter/PostGet.jsx
+++ src/components/Home-page/SectionCenter/PostGet.jsx
@@ -16,6 +16,7 @@ import { PostComments } from "./PostComments";
 import { InputComment } from "./InputComment";
 import { Container, Row, Col } from "react-bootstrap";
 import { AiFillFilter } from "react-icons/ai";
+import MyFriends from "./../../MyFriends/MyFriends";
 
 export const PostGet = () => {
   //const data= fetch api post get
@@ -24,6 +25,9 @@ export const PostGet = () => {
   const seeComment = useSelector((state) => state.linkedin.allComments);
   const [clickedComment, setClickedComment] = useState(false);
   const NUMBER_TO_DISPLAY = 4;
+  const myFriends = useSelector((state) => state.linkedin.allfriends);
+
+  const postFriends = [];
 
   useEffect(() => {
     dispatch(getPostAction());
@@ -35,29 +39,27 @@ export const PostGet = () => {
     else setClickedComment(true);
   };
 
-  const filterFunction = () => {
-    seePost = seePost.filter((el) => el);
-  };
+  for (let i = 0; i < myFriends.length; i++) {
+    postFriends.push(seePost.filter((el) => el?.user?._id === myFriends[i].id));
+  }
+
+  console.log("sono post firends", postFriends);
 
   return (
     <>
       <Container>
         <Row className="d-flex justify-content-end">
           <Col xs={1}>
-            <AiFillFilter onClick={() => filterFunction()}></AiFillFilter>
+            <AiFillFilter></AiFillFilter>
           </Col>
         </Row>
       </Container>
-      {seePost
-
-        ?.slice(-10)
-        .map((el) => {
-          return (
+      {postFriends
+        ?.map((el) =>
+          el.map((el) => (
             <Card key={el?._id} className="mt-2">
               <Card.Body className="bordo p-0 m-0 mb-0 border-bottom pb-1">
                 <div className="d-flex justify-content-between mb-2 header-post mt-3 mx-3">
-                  {/* src={data.user.image} */}
-
                   <div className="d-flex">
                     <div className="d-flex">
                       <Image
@@ -69,12 +71,9 @@ export const PostGet = () => {
                         <Card.Title className="nome-utente mb-0">
                           {el.user?.name} {el.user?.surname}
                         </Card.Title>
-                        <Card.Text className="text-secondary lavoro-utente mb-0">
-                          {el.user?.title}
-                        </Card.Text>
+                        <Card.Text className="text-secondary lavoro-utente mb-0">{el.user?.title}</Card.Text>
                         <Card.Text className="text-secondary data-post d-flex align-items-center">
-                          {el?.createdAt?.slice(0, -14)}{" "}
-                          {el?.createdAt?.slice(11, 16)} •
+                          {el?.createdAt?.slice(0, -14)} {el?.createdAt?.slice(11, 16)} •
                           <BiWorld className="ms-1 icon-world" />
                         </Card.Text>
                       </div>
@@ -86,11 +85,7 @@ export const PostGet = () => {
                 </div>
                 <Card.Text className=" mx-3">{el?.text}</Card.Text>
                 {el?.image ? (
-                  <Image
-                    src={el?.image}
-                    style={{ width: "100%", height: "30%" }}
-                    className="mb-3"
-                  />
+                  <Image src={el?.image} style={{ width: "100%", height: "30%" }} className="mb-3" />
                 ) : (
                   <Image src={el?.image} className="d-none" />
                 )}
@@ -98,9 +93,7 @@ export const PostGet = () => {
                 <div className="d-flex justify-content-between mx-3">
                   <span className="d-flex align-items-center likes">
                     <SlLike className="me-1 text-primary" />
-                    <small className="num-like text-secondary">
-                      {Math.floor(Math.random() * 100)}
-                    </small>
+                    <small className="num-like text-secondary">{Math.floor(Math.random() * 100)}</small>
                   </span>
                   <small className="likes num-like text-secondary">
                     {Math.floor(Math.random() * 300)} diffusioni post
@@ -110,37 +103,29 @@ export const PostGet = () => {
               <Card.Body className="d-flex justify-content-between p-0 me-3 ms-3">
                 <Button className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover">
                   <SlLike className="me-1 fw-bold" />
-                  <p className="d-none d-md-flex align-items-center m-0">
-                    Consiglia
-                  </p>
+                  <p className="d-none d-md-flex align-items-center m-0">Consiglia</p>
                 </Button>
                 <Button
                   className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover"
                   onClick={() => handleClikcked()}
                 >
                   <FaRegCommentDots className="me-1 fw-bold" />
-                  <p className="d-none d-md-flex align-items-center m-0">
-                    Commenta
-                  </p>
+                  <p className="d-none d-md-flex align-items-center m-0">Commenta</p>
                 </Button>
                 <Button className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover">
                   <BsArrowRepeat className="me-1 fw-bold" />
-                  <p className="d-none d-md-flex align-items-center m-0">
-                    Diffondi il post
-                  </p>
+                  <p className="d-none d-md-flex align-items-center m-0">Diffondi il post</p>
                 </Button>
                 <Button className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover">
                   <RiSendPlaneFill className="me-1 fw-bold" />
-                  <p className="d-none d-md-flex align-items-center m-0">
-                    Invia
-                  </p>
+                  <p className="d-none d-md-flex align-items-center m-0">Invia</p>
                 </Button>
               </Card.Body>
               <PostComments id={el._id} />
               <InputComment id={el._id} />
             </Card>
-          );
-        })
+          ))
+        )
         .reverse()}
     </>
   );
