c3543b7febb148df18359a3e99be806eef3823e4
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-09 12:37:43 +0100

    input comment fixed css

--- src/components/Home-page/SectionCenter/InputComment.jsx
+++ src/components/Home-page/SectionCenter/InputComment.jsx
@@ -1,23 +1,30 @@
 import { Form, Button } from "react-bootstrap";
 import { postCommentAction, getCommentAction } from "../../../redux/action";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 
 export const InputComment = (props) => {
   const dispatch = useDispatch();
   const commentsObj = { comment: "", rate: 1, elementId: props.id };
   const [comment, setComment] = useState(commentsObj);
+  const pictureProfile = useSelector((state) => state.linkedin.profile);
+  console.log("IMMAGINI", pictureProfile);
 
   return (
-    <div className="d-flex">
+    <div className="d-flex align-items-center">
+      <img className="rounded-circle ms-2" src={pictureProfile.image} alt="" />
       <Form.Control
+        style={{ borderRadius: "30px" }}
         type="text"
+        className="p-1 m-2"
         placeholder="Aggiungi un commento... "
         onChange={(e) => {
           setComment((prev) => ({ ...prev, comment: e.target.value }));
         }}
       />
       <Button
+        style={{ borderRadius: "30px" }}
+        className="me-2"
         onClick={() => {
           dispatch(postCommentAction(comment));
           dispatch(getCommentAction());
--- src/components/Home-page/SectionCenter/PostGet.jsx
+++ src/components/Home-page/SectionCenter/PostGet.jsx
@@ -15,7 +15,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { AiFillFilter } from "react-icons/ai";
 import { Pagination } from "./Pagination";
 
-export const PostGet = () => {
+export const PostGet = (props) => {
   const dispatch = useDispatch();
   const seePost = useSelector((state) => state.linkedin.allPost);
   const [clickedComment, setClickedComment] = useState(false);
@@ -38,7 +38,10 @@ export const PostGet = () => {
 
   const filtredPosts = seePost
     .filter((singlePost) => {
-      return myFriends?.includes(singlePost.user?._id) || singlePost.user?._id === "63fcc323f193e60013807f6a";
+      return (
+        myFriends?.includes(singlePost.user?._id) ||
+        singlePost.user?._id === "63fcc323f193e60013807f6a"
+      );
     })
     .reverse();
 
@@ -76,9 +79,16 @@ export const PostGet = () => {
                     <Card.Title className="nome-utente mb-0">
                       {el.user?.name} {el.user?.surname}
                     </Card.Title>
-                    <Card.Text className="text-secondary lavoro-utente mb-0">{el.user?.title}</Card.Text>
+                    <Card.Text className="text-secondary lavoro-utente mb-0">
+                      {el.user?.title}
+                    </Card.Text>
                     <Card.Text className="text-secondary data-post d-flex align-items-center">
-                      {el?.createdAt?.slice(0, -14).split("-").reverse().join("-")} {el?.createdAt?.slice(11, 16)} •
+                      {el?.createdAt
+                        ?.slice(0, -14)
+                        .split("-")
+                        .reverse()
+                        .join("-")}{" "}
+                      {el?.createdAt?.slice(11, 16)} •
                       <BiWorld className="ms-1 icon-world" />
                     </Card.Text>
                   </div>
@@ -90,33 +100,47 @@ export const PostGet = () => {
             </div>
             <Card.Text className=" mx-3">{el?.text}</Card.Text>
             {el?.image ? (
-              <Image src={el?.image} style={{ width: "100%", height: "30%" }} className="mb-3" />
+              <Image
+                src={el?.image}
+                style={{ width: "100%", height: "30%" }}
+                className="mb-3"
+              />
             ) : (
               <Image src={el?.image} className="d-none" />
             )}
             <div className="d-flex justify-content-between mx-3">
               <span className="d-flex align-items-center likes">
                 <SlLike className="me-1 text-primary" />
-                <small className="num-like text-secondary">{Math.floor(Math.random() * 100)}</small>
+                <small className="num-like text-secondary">
+                  {Math.floor(Math.random() * 100)}
+                </small>
               </span>
-              <small className="likes num-like text-secondary">{Math.floor(Math.random() * 300)} diffusioni post</small>
+              <small className="likes num-like text-secondary">
+                {Math.floor(Math.random() * 300)} diffusioni post
+              </small>
             </div>
           </Card.Body>
           <Card.Body className="d-flex justify-content-between p-0 me-3 ms-3">
             <Button className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover">
               <SlLike className="me-1 fw-bold" />
-              <p className="d-none d-md-flex align-items-center m-0">Consiglia</p>
+              <p className="d-none d-md-flex align-items-center m-0">
+                Consiglia
+              </p>
             </Button>
             <Button
               className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover"
               onClick={() => handleClikcked()}
             >
               <FaRegCommentDots className="me-1 fw-bold" />
-              <p className="d-none d-md-flex align-items-center m-0">Commenta</p>
+              <p className="d-none d-md-flex align-items-center m-0">
+                Commenta
+              </p>
             </Button>
             <Button className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover">
               <BsArrowRepeat className="me-1 fw-bold" />
-              <p className="d-none d-md-flex align-items-center m-0">Diffondi il post</p>
+              <p className="d-none d-md-flex align-items-center m-0">
+                Diffondi il post
+              </p>
             </Button>
             <Button className="text-secondary bg-transparent border-0 d-flex align-items-center mt-1 mb-1 bottone-hover">
               <RiSendPlaneFill className="me-1 fw-bold" />
@@ -127,7 +151,11 @@ export const PostGet = () => {
           <InputComment id={el._id} />
         </Card>
       ))}
-      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 };
